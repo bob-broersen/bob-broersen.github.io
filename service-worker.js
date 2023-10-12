e2488ac4b1501957a779be8248766d23447349b3
bob-broersen <70572220+bob-broersen@users.noreply.github.com>
2023-10-12 14:05:55 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,3 +3,8 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 
 // Gebruik `workbox.routing.registerRoute()`...
 
+workbox-routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.NetworkFirst({
+    cacheName:'CACHEKEY'
+  });

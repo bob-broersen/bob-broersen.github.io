3281ee7df7a2027d57141a1359d9312f13b5fdee
bob-broersen <70572220+bob-broersen@users.noreply.github.com>
2023-10-12 14:09:28 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-// Gebruik `workbox.routing.registerRoute()`...
+// Gebruik `workbox.routing.registerRoute()`... test
 
 workbox-routing.registerRoute(
   new RegExp('/*'),
